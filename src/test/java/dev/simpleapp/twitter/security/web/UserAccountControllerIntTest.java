bc650b031498189ad338ea58f39f74878a0a2684
Sergey Labuzov <s.labuzov.public@gmail.com>
2026-08-25 14:52:58 +0300

    Bump to Spring Boot 4.1.0

--- src/test/java/dev/simpleapp/twitter/security/web/UserAccountControllerIntTest.java
+++ src/test/java/dev/simpleapp/twitter/security/web/UserAccountControllerIntTest.java
@@ -7,7 +7,7 @@ import dev.simpleapp.twitter.security.web.model.RegisterRequest;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
